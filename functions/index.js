23c1421918706424e31b71a834ddabc1ba691aa1
yongsanglee <restart916@gmail.com>
2019-05-20 21:59:32 +0200

    update read time log

--- functions/index.js
+++ functions/index.js
@@ -50,27 +50,64 @@ exports.addTimeStamp = functions.firestore
   }, {merge: true});
 });
 
+exports.add_time_read_time_logs = functions.firestore
+  .document('read_time_logs/{document_id}')
+  .onCreate((snap, context) => {
+
+  const newValue = snap.data();
+
+  if ('createdAt' in newValue) return snap;
+
+  return snap.ref.set({
+    createdAt: context.timestamp
+  }, {merge: true});
+});
+
+updateReadLog = async () => {
+  let today = moment().format('YYYY-MM-DD');
+  let docs = await db.collection('read_logs').where("createdAt", ">=", today).get();
+
+  let count_data = {};
+  for (let doc of docs.docs) {
+      // console.log(doc.id, " => ", doc.data())
+      let read_info = doc.data();
+      let book_id = read_info.book_id;
+      if (book_id in count_data) {
+        count_data[book_id] += 1;
+      } else {
+        count_data[book_id] = 1;
+      }
+  }
+
+  db.collection('dayly_total').doc(today).set({total_count: count_data});
+};
+
+updateReadTimeLog = async () => {
+  let today = moment().format('YYYY-MM-DD');
+  let docs = await db.collection('read_time_logs').where("createdAt", ">=", today).get();
+
+  let count_data = {};
+  for (let doc of docs.docs) {
+      // console.log(doc.id, " => ", doc.data())
+      let read_info = doc.data();
+      let book_id = read_info.book_id;
+      if (book_id in count_data) {
+        count_data[book_id] += read_info.read_time;
+      } else {
+        count_data[book_id] = read_info.read_time;
+      }
+  }
+
+  db.collection('dayly_total_time').doc(today).set({total_count: count_data});
+};
+
 exports.hourly_job = functions.pubsub
   .topic('hourly-tick')
   .onPublish(async (message) => {
   console.log("This job is run every hour!");
 
-  let today = moment().format('YYYY-MM-DD')
-  let docs = await db.collection('read_logs').where("createdAt", ">=", today).get()
-
-  let count_data = {}
-  for (doc of docs.docs) {
-      // console.log(doc.id, " => ", doc.data())
-      let read_info = doc.data()
-      let book_id = read_info['book_id']
-      if (book_id in count_data) {
-        count_data[book_id] += 1
-      } else {
-        count_data[book_id] = 1
-      }
-  }
-
-  db.collection('dayly_total').doc(today).set({total_count: count_data})
+  await updateReadLog();
+  await updateReadTimeLog();
 
   return true;
 });
