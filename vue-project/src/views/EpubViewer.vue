36dc4ada5a1da9991e022c735de59cf3697d0da1
yongsang <restart916@gmail.com>
2020-12-31 15:15:25 +0900

    init page with cfi

--- vue-project/src/views/EpubViewer.vue
+++ vue-project/src/views/EpubViewer.vue
@@ -1,12 +1,5 @@
 <template>
   <div>
-    <!-- <pull-to
-      :distance-index="1"
-      :top-load-method="gotoPrev"
-      :bottom-load-method="gotoNext"
-      :top-config="topConfig"
-      :bottom-config="bottomConfig"> -->
-
     <PullToRefresh
       :down="1"
       :up="1"
@@ -18,6 +11,7 @@
       :propsLableUp="lableUp"
       :propsLableDown="lableDown"
       ref="pulltorefresh"
+      v-show="isShow"
       >
       <div v-if="loading == false">
         로딩 중입니다. - {{ message }}
@@ -37,9 +31,12 @@
         <button @click="fontSizeUp"> 가+ </button>
         <button @click="changeThmem('normal')"> 노말 </button>
         <button @click="changeThmem('dark')"> 다크 </button>
+        <button @click="marginDown"> 여백- </button>
+        <button @click="marginUp"> 여백+ </button>
+        <!-- <button @click="changeFlow('scrolled')"> 쭉 </button>
+        <button @click="changeFlow('scrolled-doc')"> 챕터 </button> -->
       </div>
     </BottomBar>
-
   </div>
 </template>
 
@@ -60,10 +57,13 @@ export default {
     let user = await fireauth.signInAnonymously();
     console.log('fireauth', user)
 
-    let book_id = this.$route.params.book_id
+    let book_id = this.$route.params.book_id;
+    let cfi = decodeURI(this.$route.query.cfi || '') || undefined;
+    // cfi = "epubcfi(/6/18[ch01-text]!/4/14/1:136)"
+    this.cfi = cfi;
 
-    console.log('mounted', book_id)
-    this.message = `mounted ${book_id}`
+    console.log('mounted', book_id, cfi)
+    this.message = `mounted ${book_id}, ${cfi}`
 
     let book = await firestore.collection('books').doc(book_id).get()
     let firestore_url = book.data().firestore_url;
@@ -87,118 +87,20 @@ export default {
     this.message = `path`
 
     this.epubBook = ePub(path);
-    let renderTarget = 'viewer'; // document.body
-    this.rendition = this.epubBook.renderTo(renderTarget, {
+    this.rendition = this.epubBook.renderTo(this.renderTarget, {
       // manager: "continuous",
       width: "100%",
       // flow:"scrolled"
       flow:"scrolled-doc"
     });
     // let rendition = this.epubBook.renderTo('viewer', { manager: "continuous", width: "100%", flow:"scrolled" });
-    this.displayed = this.rendition.display();
-
-    this.displayed.then((renderer) => {
-      // -- do stuff
-      console.log('this.displayed.then');
-      this.loading = true;
-    });
 
     // Navigation loaded
     this.epubBook.loaded.navigation.then(function(toc){
       console.log('this.epubBook.loaded.navigation', toc);
     });
 
-    this.rendition.on("touchstart", (e) => {
-      e.preventDefault();
-      // console.log('rendition touchstart', e);
-      this.$refs.pulltorefresh.touchStart(e);
-    });
-
-    this.rendition.on("touchmove", (e) => {
-      e.preventDefault();
-      // console.log('rendition touchmove', e);
-      this.$refs.pulltorefresh.touchMove(e);
-    });
-
-    this.rendition.on("touchend", (e) => {
-      e.preventDefault();
-      // console.log('rendition touchend', e);
-      this.$refs.pulltorefresh.touchEnd(e);
-    });
-
-    this.rendition.on("relocated", function(location){
-      console.log('relocated', location, location.start.cfi);
-      window.scrollTo(0,0);
-
-      if (window.flutter_webview) {
-        window.flutter_webview.postMessage(`relocated:${location.start.cfi}`);
-      }
-    });
-
-    this.rendition.on("rendered", function(section){
-      console.log('rendered', section);
-      var nextSection = section.next();
-      var prevSection = section.prev();
-
-      // if (window.flutter_webview) {
-      //   window.flutter_webview.postMessage(`rendered`);
-      // }
-
-      // if(nextSection) {
-      //   let nextNav = this.epubBook.navigation.get(nextSection.href);
-      //   let nextLabel = "next";
-      //
-      //   if(nextNav) {
-      //     nextLabel = nextNav.label;
-      //   }
-      //
-      //   next.textContent = nextLabel + " »";
-      // } else {
-      //   next.textContent = "";
-      // }
-      //
-      // if(prevSection) {
-      //   let prevNav = this.epubBook.navigation.get(prevSection.href);
-      //   let prevLabel = "previous";
-      //
-      //   if(prevNav) {
-      //     prevLabel = prevNav.label;
-      //   }
-      //
-      //   prev.textContent = "« " + prevLabel;
-      // } else {
-      //   prev.textContent = "";
-      // }
-    });
-
-    this.rendition.themes.register(
-      "normal",
-      {
-        "body": { "background-color": "inherit" },
-        "html": { "-webkit-filter": "inherit", "filter": "inherit" },
-        "img": {
-          "-webkit-filter": "inherit",
-          "filter": "inherit",
-          "max-width": "100% !important;",
-          "max-height": "100% !important;"
-        }
-      }
-    );
-
-    this.rendition.themes.register(
-      "dark",
-      {
-        "body": { "background-color": "#111111" },
-        "html": { "-webkit-filter": "invert(1) hue-rotate(180deg)", "filter": "invert(1) hue-rotate(180deg)" },
-        "img": {
-          "-webkit-filter": "invert(1) hue-rotate(180deg)",
-          "filter": "invert(1) hue-rotate(180deg)",
-          "max-width": "100% !important;",
-          "max-height": "100% !important;"
-        }
-      }
-    );
-
+    this.initRendition();
     this.message = `end`
   },
   data () {
@@ -220,7 +122,12 @@ export default {
         loading: '로딩중',
         complete: '로딩완료',
       },
-      fontSize: 100
+
+      isShow: true,
+      renderTarget: 'viewer', // document.body
+      fontSize: 100,
+      sideMargin: 20,
+      cfi: undefined,
     }
   },
   methods: {
@@ -270,12 +177,155 @@ export default {
     updateFontSize() {
       if (this.rendition) {
         this.rendition.themes.default({ "p": { "font-size": `${this.fontSize}% !important`}})
-        console.log(`this.rendition ${this.rendition.width}`);
+      }
+    },
+    marginUp() {
+      this.sideMargin += 10
+      this.updateSideMargin()
+    },
+    marginDown() {
+      this.sideMargin = Math.max(0, this.sideMargin - 10)
+      this.updateSideMargin()
+      console.log('marginDown');
+    },
+    updateSideMargin() {
+      if (this.rendition) {
+        this.rendition.themes.default({ "body": { "padding": `0px ${this.sideMargin}px !important`}})
+        console.log('updateSideMargin', this.sideMargin);
       }
     },
     changeThmem(theme) {
-      this.rendition.themes.select(theme);
-      this.rendition.start()
+      if (this.rendition) {
+        this.rendition.themes.select(theme);
+        this.rendition.start()
+      }
+    },
+    changeFlow(flow) {
+      if (this.rendition) {
+        this.rendition.destroy()
+        if (flow == 'scrolled') {
+          this.rendition = this.epubBook.renderTo(document.body, {
+            manager: "continuous",
+            width: "100%",
+            flow:"scrolled"
+          });
+          this.isShow = false
+        } else {
+          this.rendition = this.epubBook.renderTo(this.renderTarget, {
+            width: "100%",
+            flow:"scrolled-doc"
+          });
+          this.isShow = true
+        }
+        this.initRendition()
+      }
+    },
+    initRendition() {
+      this.displayed = this.rendition.display(this.cfi);
+
+      this.displayed.then((renderer) => {
+        // -- do stuff
+        console.log('this.displayed.then');
+        this.loading = true;
+      });
+
+      this.rendition.on("touchstart", (e) => {
+        e.preventDefault();
+        // console.log('rendition touchstart', e);
+        this.$refs.pulltorefresh.touchStart(e);
+      });
+
+      this.rendition.on("touchmove", (e) => {
+        e.preventDefault();
+        // console.log('rendition touchmove', e);
+        this.$refs.pulltorefresh.touchMove(e);
+      });
+
+      this.rendition.on("touchend", (e) => {
+        e.preventDefault();
+        // console.log('rendition touchend', e);
+        this.$refs.pulltorefresh.touchEnd(e);
+      });
+
+      this.rendition.on("relocated", function(location){
+        this.cfi = location.end.cfi;
+        console.log('relocated', location, this.cfi);
+
+        if (this.isShow) {
+          window.scrollTo(0,0);
+        }
+
+        if (window.flutter_webview) {
+          window.flutter_webview.postMessage(`relocated:${this.cfi}`);
+        }
+      });
+
+      this.rendition.on("rendered", function(section){
+        // console.log('rendered', section);
+        var nextSection = section.next();
+        var prevSection = section.prev();
+
+        // if (window.flutter_webview) {
+        //   window.flutter_webview.postMessage(`rendered`);
+        // }
+
+        // if(nextSection) {
+        //   let nextNav = this.epubBook.navigation.get(nextSection.href);
+        //   let nextLabel = "next";
+        //
+        //   if(nextNav) {
+        //     nextLabel = nextNav.label;
+        //   }
+        //
+        //   next.textContent = nextLabel + " »";
+        // } else {
+        //   next.textContent = "";
+        // }
+        //
+        // if(prevSection) {
+        //   let prevNav = this.epubBook.navigation.get(prevSection.href);
+        //   let prevLabel = "previous";
+        //
+        //   if(prevNav) {
+        //     prevLabel = prevNav.label;
+        //   }
+        //
+        //   prev.textContent = "« " + prevLabel;
+        // } else {
+        //   prev.textContent = "";
+        // }
+      });
+
+      this.rendition.themes.register(
+        "normal",
+        {
+          "body": { "background-color": "inherit" },
+          "html": { "-webkit-filter": "inherit", "filter": "inherit" },
+          "img": {
+            "-webkit-filter": "inherit",
+            "filter": "inherit",
+            "max-width": "100% !important;",
+            "max-height": "100% !important;"
+          }
+        }
+      );
+
+      this.rendition.themes.register(
+        "dark",
+        {
+          "body": { "background-color": "#111111" },
+          "html": { "-webkit-filter": "invert(1) hue-rotate(180deg)", "filter": "invert(1) hue-rotate(180deg)" },
+          "img": {
+            "-webkit-filter": "invert(1) hue-rotate(180deg)",
+            "filter": "invert(1) hue-rotate(180deg)",
+            "max-width": "100% !important;",
+            "max-height": "100% !important;"
+          }
+        }
+      );
+
+      this.updateSideMargin()
+      this.updateFontSize()
     }
   }
 }
